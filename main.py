7cb3d96b289cee18a0be08391c8ee548e2da09e5
jasmi_ugm7gn6 <singjasm@oregonstate.edu>
2022-10-26 23:23:43 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Author: Jasmine Singh
+# Github username: Jassig98
+# Date: October 26, 2022
+# Description: Taxicab class, 3 private data members
+
+class Taxicab:
+    def __init__(self,x,y):
+        self.x_coordinate = x
+        self.y_coordinate = y
+        self.odometer = 0
+
+    def move_x(self,value):
+        self.x_coordinate += distance
+        self.odometer += abs(distance)
+
+    def move_y(self,value):
+        self.y_coordinate += distance
+        self.odometer += abs(distance)
+
+    def get_x_coordinate(self):
+        return self.x_coordinate
+
+    def get_y_coordinate(self):
+        return self.y_coordinate
+
+    def get_odometer(self):
+        return self.odometer
+
+
